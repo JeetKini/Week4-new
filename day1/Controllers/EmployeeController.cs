93cb75912b91ab65ca28a7e6be7a713330d534c0
JeetKini <151983803+JeetKini@users.noreply.github.com>
2023-12-18 18:32:49 +0530

    Add files via upload

---
+++ day1/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication19.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+
+        [Route("Employee")]
+        public ActionResult GetList()
+        {
+            return View();
+        }
+        [Route("NewEmployee")]
+        public ActionResult AddEmloyee()
+        {
+            return View();
+        }
+    }
+}
---
+++ day1/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication18.Models
+{
+    public class Employee
+    {
+        [Required]
+        public string Name  { get; set; }
+
+        [JeetValidation (ErrorMessage ="THE name jeet is mendetory")]
+        public string Address { get; set; }
+
+    }
+}
