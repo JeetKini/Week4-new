5616145dc1acae51ead720d51834cacd0831e8d6
JeetKini <151983803+JeetKini@users.noreply.github.com>
2023-12-19 18:22:11 +0530

    Add files via upload

---
+++ day2/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication19.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+
+        [Route("Employee")]
+        public ActionResult GetList()
+        {
+            return View("GetList","NewLayout");
+        }
+        [Route("NewEmployee")]
+        public ActionResult AddEmloyee()
+        {
+            return View("AddEmloyee","NewLayout" );
+        }
+    }
+}
